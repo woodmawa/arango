9e5136e346adb891a7de898485d3f9df9893a648
woodmawa <will.woodman@btinternet.com>
2019-11-03 22:11:34 +0000

    fiddling with libraries

--- src/main/groovy/com/softwood/arango/QueryRunner.groovy
+++ src/main/groovy/com/softwood/arango/QueryRunner.groovy
@@ -1,14 +1,17 @@
 package com.softwood.arango
 
 import com.arangodb.springframework.core.ArangoOperations
+import com.softwood.arango.model.Organisation
 import com.softwood.arango.repository.OperatesFromManyRepository
 import com.softwood.arango.repository.OrganisationRepository
 import com.softwood.arango.repository.SiteRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.domain.Sort
 
 @ComponentScan("com.softwood.arango")
-class QueryRunner {
+class QueryRunner implements CommandLineRunner{
     @Autowired
     private ArangoOperations operations
     @Autowired
@@ -20,11 +23,20 @@ class QueryRunner {
     @Autowired
     private OperatesFromManyRepository ownsRepo  //edge relationship
 
+
     @Override
     public void run (final String... args) {
         println "--- running  query runner ---"
 
-        println "numbers of organisations : " + orgRepo.count()
+        long orgCount
+        println "numbers of organisations : " + (orgCount = orgRepo.count())
 
+        //query using the operations bean
+        long count = operations.collection(Organisation.class).count()
+        assert count == orgCount
+
+        println("## Return all Organisations sorted by name")
+        Iterable<Organisation> allSorted = orgRepo.findAll(new Sort(new Sort.Order(Sort.Direction.ASC, "name")))
+        allSorted.forEach(System.out.&println)
     }
 }
