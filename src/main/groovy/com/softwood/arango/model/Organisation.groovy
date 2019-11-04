c3cd165fa7536a9ae3a875b8474377da7538914e
woodmawa <will.woodman@btinternet.com>
2019-11-04 18:47:49 +0000

    tried 1:m org to site

--- src/main/groovy/com/softwood/arango/model/Organisation.groovy
+++ src/main/groovy/com/softwood/arango/model/Organisation.groovy
@@ -11,12 +11,23 @@ import com.arangodb.springframework.annotation.Document
 @HashIndex(fields=["name"], unique=true) //set unique index
 public class Organisation {
 
+    enum OrgType {
+        CSP ('Communications Service provider', "telco "),
+        Bank ('bank', "financial service prover"),
+        Other ('unspecified', "dont know type")
+
+        final String id
+        final String description
+        static final Map map
+    }
+
     @Id
     private String id
 
     private String name
     private String webAddress
     private Integer inaugurated
+    private OrgType type
 
 
     @Relations (edges = OperatesFromMany.class, maxDepth=1, direction= Relations.Direction.ANY, lazy=true)
