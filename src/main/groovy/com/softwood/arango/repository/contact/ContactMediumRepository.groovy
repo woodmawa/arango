8fa06a659e1dcd69e1ef00f71f85b75b3566219e
woodmawa <will.woodman@btinternet.com>
2020-07-27 15:46:26 +0100

    tried 1:m org to site

---
+++ src/main/groovy/com/softwood/arango/repository/contact/ContactMediumRepository.groovy
@@ -0,0 +1,16 @@
+package com.softwood.arango.repository.contact
+
+
+import com.arangodb.springframework.repository.ArangoRepository
+import com.softwood.arango.model.contact.Contact
+
+//needs the class type, and type of field tagged with @Id in model
+public interface ContactMediumRepository extends ArangoRepository<Contact, String> {
+
+    Contact findByType(String typeName)
+
+//    Contact getPrefferedContactMedium (String name)
+
+//    List<Contact> findByNameIgnoreCase (String name)
+
+}
---
+++ src/main/groovy/com/softwood/arango/repository/contact/ContactRepository.groovy
@@ -0,0 +1,22 @@
+package com.softwood.arango.repository.contact
+
+import com.arangodb.ArangoCursor
+import com.arangodb.ArangoDB
+import com.arangodb.ArangoDatabase
+import com.arangodb.springframework.annotation.BindVars
+import com.arangodb.springframework.annotation.Query
+import com.arangodb.springframework.repository.ArangoRepository
+import com.arangodb.velocypack.module.jdk8.VPackJdk8Module
+import com.softwood.arango.model.Customer
+import com.softwood.arango.model.Site
+import com.softwood.arango.model.contact.Contact
+import org.springframework.data.repository.query.Param
+
+//needs the class type, and type of field tagged with @Id in model
+public interface ContactRepository extends ArangoRepository<Contact, String> {
+
+    Contact findByName(String name)
+
+    List<Contact> findByNameIgnoreCase (String name)
+
+}
